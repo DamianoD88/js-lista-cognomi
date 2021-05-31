879837c21ccda45a3ca860d9473cdc82fd7cc437
DamianoD88 <dispe88@live.it>
2021-05-31 15:07:34 +0200

    add init

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// Mail
+// Chiedi all’utente la sua email,
+//  2.  controlla che sia nella lista di chi può accedere,
+//  3.  stampa un messaggio appropriato sull’esito del controllo.
+
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
+// Consigli del giorno:
+// Consultiamo la documentazione W3Schools o MDN per trovare i metodi JavaScript che possono esserci utili.
+// Buon lavoro e buon divertimento! 
+
+
+var mail = prompt('la tua mail?');
