88f82db5b25dfccbf13e6d898be026661970e650
DamianoD88 <dispe88@live.it>
2021-05-31 16:52:28 +0200

    second file js

---
+++ js/scritp-list.js
@@ -0,0 +1,13 @@
+// Lista Cognomi:
+// chiedi all’utente il cognome
+// inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+// stampa la lista ordinata alfabeticamente
+// scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
+// Bonus: (da fare solo dopo aver concluso tutto) Trovate un modo per trasformare in maiuscolo la prima lettera del cognome inserito dall'utente e tutto il resto in minuscolo.
+// Consigli del giorno:
+// Consultiamo la documentazione W3Schools o MDN per trovare i metodi JavaScript che possono esserci utili.
+// Buon lavoro e buon divertimento! 
+
+// chiedo all'utenet il cognome tramite il promt
+
+var cognome = prompt('cognome');
